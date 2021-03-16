35c5acf523cd166e1fef46d12c30dedd885b0c45
= <=>
2021-03-16 13:36:45 +0100

    actualización instantanea de las contraseñas guardadas localmente

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 import { IonSlides } from '@ionic/angular';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { user } from 'src/app/model/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { CryptingService } from 'src/app/services/crypting.service';
@@ -17,7 +18,7 @@ import { environment } from 'src/environments/environment';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit{
+export class LoginPage implements OnInit {
 
   @ViewChild('loginSlide') slide: IonSlides;
   constructor(
@@ -30,16 +31,26 @@ export class LoginPage implements OnInit{
     , private settingsSvc:SettingsService
     , private cryptingSvc:CryptingService
     ) { }
-
     
-  noPwSyncParam:any
+  noPwSyncParam:any;
   enabledBioAccess:boolean;
   async ngOnInit(): Promise<void> {
     this.biometricAuth();    
-    const numStoragedCredential = await this.lsCredentialsSvc.numStoragedCredential();
-    const password:string = (await this.utils.trans('pw')).toLowerCase();
-    if(numStoragedCredential === 1) this.noPwSyncParam = {numCredentials: `1 ${password}`}
-    if(numStoragedCredential > 1) this.noPwSyncParam = {numCredentials: numStoragedCredential + `${password}s`}
+    this.showNumLocalCredentials();
+  }
+  
+  private async showNumLocalCredentials() {
+    const $numCreds = this.lsCredentialsSvc.getNumStoragedCredentials();
+    await this.lsCredentialsSvc.numStoragedCredential();
+    
+    $numCreds.subscribe(async numStoragedCredential => {
+      console.log("$numCreds.subscribe");      
+      const password: string = (await this.utils.trans('pw')).toLowerCase();
+      if (numStoragedCredential === 1)
+      this.noPwSyncParam = { numCredentials: `1 ${password}` };
+      if (numStoragedCredential > 1)
+      this.noPwSyncParam = { numCredentials: numStoragedCredential + ` ${password}s` };
+    })    
   }
 
   async ionViewDidEnter() {    
--- src/app/services/local-storaged-credentials.service.ts
+++ src/app/services/local-storaged-credentials.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Plugins } from '@capacitor/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { Credential } from '../model/credential';
 import { UiService } from './ui.service';
 const { Storage } = Plugins;
@@ -38,15 +39,26 @@ export class LocalStoragedCredentialsService {
         key: this.storageName,
         value: JSON.stringify(this.localCredentials)
       });
+      this.encreaseNumStoragedCredentials()
       this.uiSvc.success('add-local-credential', 4000);
     } catch(e){
       this.uiSvc.error(e);
     }
   }
 
-  async numStoragedCredential():Promise<number> {
+  private $numCredential = new BehaviorSubject(0);
+  private numCredential:number;
+
+  async numStoragedCredential():Promise<void> {
     await this.loadLocalCredentials();
-    return this.localCredentials?.length || 0;
+    this.numCredential = this.localCredentials?.length || 0;
+    this.$numCredential.next(this.numCredential);
+  }
+  getNumStoragedCredentials = ():BehaviorSubject<number> => {return this.$numCredential};
+
+  encreaseNumStoragedCredentials(num:number = 1):void{
+    this.numCredential+=num;
+    this.$numCredential.next(this.numCredential);
   }
 
   clean(){
