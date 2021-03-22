960b219ca1a0cebb8ad69f10076756b834907258
= <=>
2021-03-22 19:22:18 +0100

    Example enviroment properties

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  cryptingKey: "RANDOM_SECRET_KEY",
+  firebaseConfig: {
+    //Start a project in firebase
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxx.firebaseapp.com",
+    projectId: "xxxxxxx",
+    storageBucket: "xxxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  cryptingKey: "RANDOM_SECRET_KEY",
+  firebaseConfig: {
+    //Start a project in firebase
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxx.firebaseapp.com",
+    projectId: "xxxxxxx",
+    storageBucket: "xxxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  }
+};
